01cf16437edac3398fce7dfa422c86d4ac719e0e
Oliver Walsh <owalsh@redhat.com>
2025-01-22 14:54:49 +0000

    Add defaults for LabelSelector/MatchLabelKeys

--- modules/common/topology/topology.go
+++ modules/common/topology/topology.go
@@ -19,13 +19,21 @@ package topology
 import (
 	"context"
 	"fmt"
+
 	topologyv1 "github.com/openstack-k8s-operators/infra-operator/apis/topology/v1beta1"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/helper"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/util"
 	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+// Standard MatchLabelKeys labels for Deployments/Statefulsets
+const (
+	StatefulsetMatchLabelKey = "controller-revision-hash"
+	DeploymentMatchLabelKey  = "pod-template-hash"
 )
 
 // EnsureTopologyRef - retrieve the Topology CR referenced and add a finalizer
@@ -34,6 +42,9 @@ func EnsureTopologyRef(
 	h *helper.Helper,
 	topologyRef *Topology,
 	finalizer string,
+	defaultLabelSelector metav1.LabelSelector,
+	defaultMatchLabelKeys []string,
+
 ) (*topologyv1.Topology, string, error) {
 
 	var err error
@@ -44,7 +55,7 @@ func EnsureTopologyRef(
 		return nil, "", fmt.Errorf("No valid TopologyRef input passed")
 	}
 
-	topology, hash, err := topologyv1.GetTopologyByName(
+	topology, _, err := topologyv1.GetTopologyByName(
 		ctx,
 		h,
 		topologyRef.Name,
@@ -53,12 +64,88 @@ func EnsureTopologyRef(
 	if err != nil {
 		return topology, hash, err
 	}
+
 	// Add finalizer (if not present) to the resource consumed by the Service
 	if controllerutil.AddFinalizer(topology, fmt.Sprintf("%s-%s", h.GetFinalizer(), finalizer)) {
 		if err := h.GetClient().Update(ctx, topology); err != nil {
 			return topology, hash, err
 		}
 	}
+
+	// Set default LabelSelector and MatchLabelKeys if applicable
+	topology = topology.DeepCopy()
+
+	topologyConstraints := topology.Spec.TopologySpreadConstraints
+	if topologyConstraints != nil {
+		for i := 0; i < len(*topologyConstraints); i++ {
+			current := &(*topologyConstraints)[i]
+			if current.LabelSelector == nil {
+				current.LabelSelector = &defaultLabelSelector
+			}
+			if len(current.MatchLabelKeys) == 0 {
+				current.MatchLabelKeys = defaultMatchLabelKeys
+			}
+		}
+	}
+	affinity := topology.Spec.Affinity
+	if affinity != nil {
+		podAffinity := affinity.PodAffinity
+		if podAffinity != nil {
+			for i := range podAffinity.RequiredDuringSchedulingIgnoredDuringExecution {
+				current := &podAffinity.RequiredDuringSchedulingIgnoredDuringExecution[i]
+				if current.LabelSelector == nil {
+					current.LabelSelector = &defaultLabelSelector
+				}
+				// Coming in kubernetes 1.31 (4.18?) https://kubernetes.io/blog/2024/08/16/matchlabelkeys-podaffinity/
+				// Should we set it anyway ... not sure
+				if len(current.MatchLabelKeys) == 0 {
+					current.MatchLabelKeys = defaultMatchLabelKeys
+				}
+			}
+			for i := range podAffinity.PreferredDuringSchedulingIgnoredDuringExecution {
+				current := &podAffinity.PreferredDuringSchedulingIgnoredDuringExecution[i].PodAffinityTerm
+				if current.LabelSelector == nil {
+					current.LabelSelector = &defaultLabelSelector
+				}
+				// Coming in kubernetes 1.31 (4.18?) https://kubernetes.io/blog/2024/08/16/matchlabelkeys-podaffinity/
+				// Should we set it anyway ... not sure
+				if len(current.MatchLabelKeys) == 0 {
+					current.MatchLabelKeys = defaultMatchLabelKeys
+				}
+			}
+		}
+		podAntiAffinity := affinity.PodAntiAffinity
+		if podAntiAffinity != nil {
+			for i := range podAntiAffinity.RequiredDuringSchedulingIgnoredDuringExecution {
+				current := &podAntiAffinity.RequiredDuringSchedulingIgnoredDuringExecution[i]
+				if current.LabelSelector == nil {
+					current.LabelSelector = &defaultLabelSelector
+				}
+				// Coming in kubernetes 1.31 (4.18?) https://kubernetes.io/blog/2024/08/16/matchlabelkeys-podaffinity/
+				// Should we set it anyway ... not sure
+				if len(current.MatchLabelKeys) == 0 {
+					current.MatchLabelKeys = defaultMatchLabelKeys
+				}
+			}
+			for i := range podAntiAffinity.PreferredDuringSchedulingIgnoredDuringExecution {
+				current := &podAntiAffinity.PreferredDuringSchedulingIgnoredDuringExecution[i].PodAffinityTerm
+				if current.LabelSelector == nil {
+					current.LabelSelector = &defaultLabelSelector
+				}
+				// Coming in kubernetes 1.31 (4.18?) https://kubernetes.io/blog/2024/08/16/matchlabelkeys-podaffinity/
+				// Should we set it anyway ... not sure
+				if len(current.MatchLabelKeys) == 0 {
+					current.MatchLabelKeys = defaultMatchLabelKeys
+				}
+			}
+		}
+	}
+
+	hash, err = util.ObjectHash(topology.Spec)
+	if err != nil {
+		return topology, hash, err
+	}
+
 	return topology, hash, nil
 }
 
